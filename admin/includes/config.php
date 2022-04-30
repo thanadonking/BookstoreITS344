2649e962ccd20907489e25e5f42e925f52203ebc
thanadonking <thanadon283o@gmail.com>
2022-04-30 16:12:20 +0700

    Final Web RealX

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,5 +1,18 @@
 <?php 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
 
-$conn=mysqli_connect("localhost","root","","db")or die("Can't Connect...");
+$config = array(
+    'host' => $server ,
+    'user' => $username ,
+    'pw' => $password,
+    'db' => $db 
+);
+//$conn = mysqli_connect($_server, $username, $password, $db);
+$conn = mysqli_connect("us-cdbr-east-05.cleardb.net", "bcc413a6fa3945", "409fbe50", "heroku_0dae6e1a1d7c0b9");
+//$conn=mysqli_connect("localhost","root","","book_store")or die("Can't Connect...");
 	
 ?>
